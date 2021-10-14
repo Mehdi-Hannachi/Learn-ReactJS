8feca43a030e6558a3d9d9966fa8b57447089d38
Firas <mehdihannachi02@gmail.com>
2021-10-13 21:17:30 -1200

    workshop react js part 1

---
+++ src/components/Main/Main.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Card} from '../Card/Card'
+import "./main.css"
+
+const data = [
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png",
+      name: "Mehdi",
+      lastName: "Hannachi",
+    },
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png",
+      name: "Mehdi",
+      lastName: "Hannachi",
+    },
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png",
+      name: "Mehdi",
+      lastName: "Hannachi",
+    },
+  ];
+const Main = () => {
+  
+
+  return (
+    <div className="cardsList">
+      {data.map((user,i) =><Card key={i}/>)}
+    </div>
+  );
+};
+
+export default Main;
